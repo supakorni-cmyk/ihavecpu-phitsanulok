62b8e498c79b6fb5d71d428abb596d6fdd6a8f39
Boom <supakorn.i@ihavecpu.com>
2025-12-17 14:35:22 +0700

    Disable Admin CC

--- netlify/functions/book.js
+++ netlify/functions/book.js
@@ -61,7 +61,7 @@ export default async(req) => {
             to: email,
             cc: finalCCList, // <-- Use the new combined list
             subject: `[ iHAVECPU x ${brand} ] Booking Confirmation at iHAVECPU Phitsanulok`,
-            html: `<h1>Thank you!</h1><p>Booking for brand "<strong>${brand}</strong>" completed.</p><h3>Positions:</h3><ul>${spotsListHtml}</ul><hr><h3> Please wait for our confirmation email</h3><br><p>Sincerely,<br>iHAVECPU Marketing</p>`
+            html: `<h1>Thank you!</h1><p>Booking for brand "<strong>${brand}</strong>" completed.</p><h3>Positions:</h3><ul>${s.name}</ul><hr><h3> Please wait for our confirmation email.</h3><br><p>Sincerely,<br>iHAVECPU Marketing</p>`
         };
         await transporter.sendMail(mailOptions);
 
